a51ac17125a094b3cd67bc5a6939ed5fc4495d0f
Peter Humburg <peter.humburg@gmail.com>
2020-05-07 13:49:54 +1000

    Check for valid input

--- R/predict_outcome.R
+++ R/predict_outcome.R
@@ -2,6 +2,7 @@
 #' @export
 predict_outcome <-
 function(fit, data, ...) {
+  stopifnot(".imp" %in% names(data))
   pred <- by(data, data$.imp, function(x) {
     predict(fit, newdata=x, type="response", ...)
   })
