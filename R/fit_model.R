20540d85b050832de3244f00781b97ec8a8a6614
Peter Humburg <peter.humburg@gmail.com>
2020-05-08 17:04:51 +1000

    Add documentation for fit_model

--- R/fit_model.R
+++ R/fit_model.R
@@ -1,10 +1,45 @@
+#' Variable selection and model fitting
+#' 
+#' Selects variables for predictive models from multiply imputed
+#' data. 
+#' 
+#' @details
+#' The variable selection and model fitting process proceeds in three stages.
+#' 1. *LASSO* regression is used to select a subset of predictors for each
+#'   imputed dataset.
+#' 2. A consensus set of predictors is determined as follows
+#'     a. The frequency with which each predictor is selected across the
+#'       imputed datasets is determined. 
+#'     b. Starting with the most frequently selected predictors, create a series
+#'       of models that include increasing numbers of predictors.
+#'     c. Select the model with the lowest AIC.
+#' 3. The consensus model parameters are estimated on all imputed datasets without penalty.
+#' 4. Model parameters are pooled to obtain the final model.
+#' 
+#' The pooled model returned by this function is useful to generate predictions from new data.
+#' If you are interested in inspecting the parameter estimates and their variances instead use
+#' `pool(ans$selected_model$fit)`, where `ans` is the object returned by this function.
+#' 
+#' @param data An object of class [mids]
+#' @param outcome The name of the outcome measure. This should be the name of a variable in `data`.
+#' @param family The [family] object to be used. May a family object or the name of the family to be used.
+#' @param s A character string indicating which value of \eqn{\lambda} should be used. Either `lambda.min`
+#' for the optimal \eqn{\lambda} determined by cross-validation or `lambda.1se` for the optimal \eqn{\lambda + 1} standard error.
+#' 
+#' @return A list with components
+#' * *selected_model*: A list with components *formula* and *fit* containing the [formula] for the selected
+#'   model and the model fits on the imputed datasets respectively.
+#' * *pooled_model*: An object of class [glm] containing the pooled parameter estimates.
+#' 
+#' @author Peter Humburg
 #' @importFrom pROC roc
 #' @importFrom pROC auc
 #' @importFrom pROC ci
 #' @importFrom fmsb NagelkerkeR2
 #' @export
 fit_model <-
-function(data, outcome, family="binomial", s="lambda.min") {
+function(data, outcome, family="binomial", s=c("lambda.min", "lambda.1se")) {
+  s <- match.arg(s)
   fit <- select_variables(data, outcome, family)
   cand_models <- candidate_models(fit, s=s, response = outcome)
   model <- select_model(cand_models, data)
