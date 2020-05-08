0bdb57ff8fb8d0a8f50b46941fa22ba4e541216b
Peter Humburg <peter.humburg@gmail.com>
2020-05-08 21:18:48 +1000

    Ensure data is clean

--- R/internals.R
+++ R/internals.R
@@ -1,7 +1,7 @@
 #' @importFrom methods is
 data_long <- function(data) {
   if(is(data, "mids")){
-    data <- complete(data, action="long")
+    data <- complete(data, action="long") %>% clean_data()
   }
   if(!".imp" %in% names(data)) {
     stop("Expected data frame of complete data exported from `mice`. Use `mice::complete(data, action='long')` to obtain data in the right format after imputation.")
