996b875e6076ff45b56d4d96cdd55fcd20d69531
Peter Humburg <peter.humburg@gmail.com>
2020-05-15 09:52:02 +1000

    Add tests for predict_outcome

---
+++ tests/testthat/test-predict_outcome.R
@@ -0,0 +1,17 @@
+context("predictions")
+
+predictions <- predict_outcome(binom_fit$pooled_model, binom_mids)
+
+test_that("output has expected structure", {
+  expect_equal(class(predictions), "matrix")
+  expect_equal(dim(predictions), c(nrow(binomData), binom_mids$m))
+})
+
+test_that("data frames are handled correctly", {
+  expect_equal(predict_outcome(binom_fit$pooled_model, mice::complete(binom_mids, action="long")), predictions)
+  expect_error(predict_outcome(binom_fit$pooled_model, binomData), "mice::complete\\(data, action='long'\\)")
+})
+
+test_that("results have not changed",{
+  expect_known_output(print(predictions), "predict_outcome.out")
+})
