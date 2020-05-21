d2643bc82b204b34d3c4287515ea665bb0fa95e9
Peter Humburg <peter.humburg@gmail.com>
2020-05-21 23:05:56 +1000

    Clean up logitgof output

--- R/performance.R
+++ R/performance.R
@@ -68,8 +68,13 @@ performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "
     perf[["hoslem"]] <- unclass(by(data, data$.imp, function(x) {
       if(length(dots)) hoslem_args <- dots[names(dots) %in% names(formals(logitgof))]
       else hoslem_args <- list()
-      tryCatch(
-        do.call(logitgof, c(list(obs=x[[outcome]], exp=x[["prediction"]]), hoslem_args)),
+      tryCatch({
+        d <- list(obs=x[[outcome]], exp=x[["prediction"]])
+        hoslem <- do.call(logitgof, c(d, hoslem_args))
+        ## clean up data record
+        hoslem$data.name <- paste0("data$", outcome, ", prediction")
+        hoslem
+        },
         error=function(e){
           warning("Hosmer-Lemeshow Test failed with error message '", e, "'")
           ans <- list(statistic=c("X-squared"=NA), parameter=c(df=NA), p.value=NA, method="Hosmer and Lemeshow test", data.name=NA, observed=NA, expected=NA, stddiffs=NA)
