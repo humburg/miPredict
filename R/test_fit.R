862cd53fd3a58f9915db5c17caca28c8e8228e92
Peter Humburg <peter.humburg@gmail.com>
2020-06-09 21:07:54 +1000

    Update documentation

--- R/test_fit.R
+++ R/test_fit.R
@@ -1,21 +1,23 @@
 #' Assessing model performance
+#' 
+#' Compute model performance metrics.
 #'
 #' @param model An object of class [glm].
 #' @param data Data to use in performance assessment.
-#' @param which Character vector indicating which performance measures should be computed. One or more of *auc*, *thresholds*, *sp*, *se*, *coords*.
+#' @param outcome Name of outcome in `data`.
+#' @param which Character vector indicating which performance measures should be computed. One or more of *auc*, *specificity*, *sensitivity*, *accuracy*, *precision*.
 #' @param bootstrap Logical indicating whether bootstrap samples should be generated from `data`.
 #' @param iter Number of bootstrap iterations to use.
 #' @param level Width of the confidence interval.
 #' 
-#' @details 
-#' Confidence intervals may be calculated for the AUC (with `which="auc"`), thresholds derived from the ROC curve of the `model` on the provided `data` (`which="thresholds"`),
-#' specificity (`which="sp"`), sensitivity (`which="se"`), and the ROC curve (`which="coords"`).
+#' @details Confidence intervals may be calculated for a range of performance metrics, including the AUC, specificity, sensitivity, accuracy, and precision. Of these all except AUC
+#' depend on the choice of classification cut-off used. The metrics reported by this function are at the cut-off which maximises the sum of sensitivity and specificity,
+#' i.e. the Youden Index.
 #' 
 #' @return A list of performance measures of the same length as `which`.
 #' @export
 #' @author Peter Humburg
 #'
-#' @examples
-test_fit <- function(model, data, which=c("auc", "specificity", "sensitivity", "accuracy", "precision"), bootstrap=FALSE, iter=2000, level=0.95){
+test_fit <- function(model, data, outcome, which=c("auc", "specificity", "sensitivity", "accuracy", "precision"), bootstrap=FALSE, iter=2000, level=0.95){
   
 }
