337d98c41cda7add8f7bb723d8fa139d492e0c1d
Peter Humburg <peter.humburg@gmail.com>
2020-06-10 11:00:43 +1000

    Enable use of mids objects

--- R/test_fit.R
+++ R/test_fit.R
@@ -3,16 +3,21 @@
 #' Compute model performance metrics.
 #'
 #' @param model An object of class [glm].
-#' @param data Data to use in performance assessment.
+#' @param data Data to use in performance assessment. Either a data frame or an object of class [mids] (see Details).
 #' @param outcome Name of outcome in `data`.
 #' @param metrics Character vector indicating which performance measures should be computed. One or more of *auc*, *specificity*, *sensitivity*, *accuracy*, *precision*.
 #' @param bootstrap Logical indicating whether bootstrap samples should be generated from `data`.
 #' @param iter Number of bootstrap iterations to use.
 #' @param level Width of the confidence interval.
 #' 
-#' @details Confidence intervals may be calculated for a range of performance metrics, including the AUC, specificity, sensitivity, accuracy, and precision. Of these all except AUC
+#' @details 
+#' # Available performance metrics:
+#' Confidence intervals may be calculated for a range of performance metrics, including the AUC, specificity, sensitivity, accuracy, and precision. Of these all except AUC
 #' depend on the choice of classification cut-off used. The metrics reported by this function are at the cut-off which maximises the sum of sensitivity and specificity,
 #' i.e. the Youden Index.
+#' 
+#' # Using multiply imputed data:
+#' If `data` is of class [mids] the imputed datasets will be collapsed into a single dataset before performance metrics are computed.
 #' 
 #' @return A list of performance measures of the same length as `metrics`.
 #' @export
@@ -20,6 +25,9 @@
 #'
 test_fit <- function(model, data, outcome, metrics=c("auc", "specificity", "sensitivity", "accuracy", "precision"), bootstrap=FALSE, iter=2000, level=0.95){
   metrics <- match.arg(metrics, several.ok = TRUE)
+  if(is(data, "mids")) {
+    data <- mice::complete(data)
+  }
   roc <- pROC::roc(data[[outcome]], predict(model, data))
   ans <- vector(mode="list", length(metrics))
   names(ans) <- metrics
