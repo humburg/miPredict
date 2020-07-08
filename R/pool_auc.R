466ed6bdf318e0423efddb009378f8e6740b047c
Peter Humburg <peter.humburg@gmail.com>
2020-07-09 09:47:49 +1000

    Improve AUC pooling

--- R/pool_auc.R
+++ R/pool_auc.R
@@ -1,6 +1,7 @@
 #' Pool AUC estimates from multiply imputed data using Rubin's rules
 #'
-#' @param roc A list of objects of class [roc]
+#' @param roc An object of class [roc]
+#' @param n Sample size
 #'
 #' @return A list with elements *estimate* and *ci* providing the pooled AUC
 #' and its 95% confidence interval respectively.
@@ -9,11 +10,8 @@
 #' @importFrom pROC var
 #' @export
 pool_auc <-
-function(roc) {
-  n <- length(roc[[1]]$cases) + length(roc[[1]]$controls)
+function(roc, n) {
   auc_pooled <- pool.scalar(sapply(roc, auc), sapply(roc, var), n)
-  est <- min(max(auc_pooled$qbar, 0), 1)
-  auc_pooled_ci <- est + c(-sqrt(auc_pooled$t), sqrt(auc_pooled$t))*1.96
-  auc_pooled_ci <- pmin(pmax(auc_pooled_ci, 0), 1)
-  list(estimate=est, ci=auc_pooled_ci)
+  auc_pooled_ci <- auc_pooled$qbar + c(-sqrt(auc_pooled$t), sqrt(auc_pooled$t))*1.96
+  list(estimate=auc_pooled$qbar, ci=auc_pooled_ci)
 }
