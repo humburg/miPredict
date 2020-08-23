521cd1ad2104a2de071a60678c60a5145d22f39b
Peter Humburg <peter.humburg@gmail.com>
2020-08-23 21:05:51 +1000

    Avoid tests timing out

--- tests/testthat/test-crossvalidate.R
+++ tests/testthat/test-crossvalidate.R
@@ -6,6 +6,8 @@ test_that("output has expected structure", {
 })
 
 test_that("leave-one-out works", {
+  skip_on_ci()
+  skip_on_cran()
   expect_warning(cv <- crossvalidate(binom_mids, "y", k=100), "(not converge)|(0 or 1)|(scale = TRUE)")
   expect_length(cv, nrow(binomData))
 })
