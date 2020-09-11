0a464516208848db54b347cb30f35060f833616c
Peter Humburg <peter.humburg@gmail.com>
2020-09-11 16:39:58 +1000

    Refactor performance code

--- R/performance.R
+++ R/performance.R
@@ -1,23 +1,25 @@
 #' Performance metrics for predictive models
 #'
-#' @param model The (pooled) model to be evaluated.
-#' @param data Data to be used in the evaluation.
+#' @param x The model to be evaluated.
 #' @param ... Further arguments to be passed to specific methods
 #'
 #' @return A list of performance metrics.
+#' @seealso [performance.binomial()], [performance.cv()]
 #' @export
 #' @rdname performance
-performance <- function(model, data, outcome, ...){
+performance <- function(x, ...){
   UseMethod("performance")
 }
 
 
+#' Performance metrics for logistic regression models
 #' 
+#' @param x The pooled logistic regression model to be evaluated.
+#' @param data Data to be used in the evaluation.
 #' @param metrics A character vector indicating the metrics to be computed
 #' @param outcome The name of the outcome measure. This should be the name of a variable in `data`.
 #' @param model_fits A list of fitted model objects. Should contain one model for each imputed dataset (required if 'r2' is requested).
 #'
-#' @rdname performance
 #' @method performance binomial
 #' @importFrom pROC roc
 #' @importFrom pROC ci
@@ -28,13 +30,13 @@ performance <- function(model, data, outcome, ...){
 #' @importFrom stats qnorm
 #' @export
 #' @include internals.R
-performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "specificity", "sensitivity", "accuracy", "precision", "brier", "r2", "hoslem"), model_fits, ...){
+performance.binomial <- function(x, data, outcome, metrics=c("roc", "auc", "specificity", "sensitivity", "accuracy", "precision", "brier", "r2", "hoslem"), model_fits, ...){
   metrics <- match.arg(metrics, several.ok = TRUE)
   dots <- list(...)
   data <- data_long(data)
   sample_size <- max(data$.id)
   
-  predictions <- predict_outcome(model, data)
+  predictions <- predict_outcome(x, data)
   attr(predictions, 'dim') <- NULL
   data$prediction <- predictions
   
@@ -49,7 +51,7 @@ performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "
       class_perf_args <- dots[names(dots) %in% names(formals(class_perf))]
     }
     perf_raw <- by(data, data$.imp, 
-                   function(x) do.call(class_perf, c(list(model=model, data=x, outcome=outcome, 
+                   function(d) do.call(class_perf, c(list(predictions=predict(x, newdata=d, type="response"), outcome=d[[outcome]], 
                                                           metrics=class_perf_metrics), class_perf_args)),
                    simplify=FALSE)
     perf_comb <- reduce(perf_raw, function(x, y){
@@ -57,14 +59,14 @@ performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "
     })
     ci_level <- if("level" %in% names(class_perf_args)) class_perf_args$level else formals(class_perf)$level
     se_factor <- qnorm(1-(1-ci_level)/2)
-    perf_size <- cbind(sensitivity=unlist(by(data, data$.imp, function(x) sum(x[[outcome]]))), 
-                   specificity=unlist(by(data, data$.imp, function(x) sum(!x[[outcome]]))),
+    perf_size <- cbind(sensitivity=unlist(by(data, data$.imp, function(d) sum(d[[outcome]]))), 
+                   specificity=unlist(by(data, data$.imp, function(d) sum(!d[[outcome]]))),
                    accuracy=sample_size)
-    perf_var <- lapply(perf_comb, function(x) perf_var(x[,2], sample_size))
+    perf_var <- lapply(perf_comb, function(p) perf_var(p[,2], sample_size))
     perf_est <- lapply(perf_comb, "[", , 2)
     perf_pooled <- mapply(pool.scalar, perf_est, perf_var, MoreArgs=list(n=sample_size))
-    perf_pooled_ci <- apply(perf_pooled, 2, function(x) c(estimate=x[["qbar"]], ci.lower=max(0, x[["qbar"]] - se_factor*sqrt(x[["t"]]/sample_size)), 
-                          ci.upper=min(1, x[["qbar"]] + se_factor*sqrt(x[["t"]]/sample_size))))
+    perf_pooled_ci <- apply(perf_pooled, 2, function(p) c(estimate=p[["qbar"]], ci.lower=max(0, p[["qbar"]] - se_factor*sqrt(p[["t"]]/sample_size)), 
+                          ci.upper=min(1, p[["qbar"]] + se_factor*sqrt(p[["t"]]/sample_size))))
     for(i in 1:length(class_perf_metrics)){
       perf[[class_perf_metrics[i]]] <- perf_pooled_ci[,i]
     }
@@ -73,18 +75,18 @@ performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "
   
   if("roc" %in% metrics) {
     perf[["roc"]] <- by(data, data$.imp, 
-                        function(x) roc(x[[outcome]], x$prediction, ci=TRUE, plot=FALSE, 
+                        function(d) roc(d[[outcome]], d$prediction, ci=TRUE, plot=FALSE, 
                                         direction="<", levels=c("0", "1")))
   }
   if("brier" %in% metrics) {
     brier <- as.list(by(data, data$.imp,
-                        function(x) BrierDecomp((x$prediction), x[[outcome]])))
+                        function(d) BrierDecomp((d$prediction), d[[outcome]])))
     perf[["brier"]] <- pool_brier(brier, sample_size)
     
   }
   if("r2" %in% metrics){
     if(!missing(model_fits)) {
-      perf[["r2"]] <- pool_r2(model, model_fits, data)
+      perf[["r2"]] <- pool_r2(x, model_fits, data)
       
     } else{
       warning("Argument 'model_fits' is missing, skipping computation of Nagelkerke's R2.")
@@ -93,7 +95,7 @@ performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "
   if("hoslem" %in% metrics){
     if(length(dots)) hoslem_args <- dots[names(dots) %in% names(formals(logitgof))]
     else hoslem_args <- list()
-    args <- c(list(model, data, outcome), hoslem_args)
+    args <- c(list(x, data, outcome), hoslem_args)
     perf[["hoslem"]] <- 
       tryCatch(do.call(pool_hoslem, args),
         error=function(e){
@@ -105,5 +107,8 @@ performance.binomial <- function(model, data, outcome, metrics=c("roc", "auc", "
     attr(perf[["hoslem"]], "call") <- NULL
   }
   
+  perf
+}
+
   perf
 }
--- tests/testthat/test-class_perf.R
+++ tests/testthat/test-class_perf.R
@@ -2,15 +2,11 @@ context("bootstrap performance estimates")
 
 metrics <- c("auc", "specificity", "sensitivity", "accuracy", "precision")
 test_that("output contains requested measures", {
-  expect_length(perf <- class_perf(binom_fit$pooled_model, binomData_complete, "y"), length(metrics))
+  expect_length(perf <- class_perf(predict(binom_fit$pooled_model, newdata=binomData_complete), binomData_complete$y), length(metrics))
   expect_named(perf, metrics)
   expect_equivalent(sapply(perf, length), rep(3, 5))
-  expect_length(perf2 <- class_perf(binom_fit$pooled_model, binomData_complete, "y", metrics=metrics[1:2]), 2)
+  expect_length(perf2 <- class_perf(predict(binom_fit$pooled_model, newdata=binomData_complete), binomData_complete$y, metrics=metrics[1:2]), 2)
   expect_named(perf2, metrics[1:2])
-  expect_length(perf_boot <- class_perf(binom_fit$pooled_model, binomData_complete, "y", metrics=metrics[1:2], bootstrap=TRUE, iter=10), 2)
+  expect_length(perf_boot <- class_perf(predict(binom_fit$pooled_model, newdata=binomData_complete), binomData_complete$y, metrics=metrics[1:2], bootstrap=TRUE, iter=10), 2)
   expect_named(perf_boot, metrics[1:2])
-})
-
-test_that("imputed data is handled correctly", {
-  expect_error(perf <- class_perf(binom_fit$pooled_model, binom_mids, "y"), "imputed")
 })
