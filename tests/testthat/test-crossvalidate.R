7e6851ebf6ec7211b769a611f8c0a1ac572cf9fd
Peter Humburg <peter.humburg@gmail.com>
2020-09-21 10:18:23 +1000

    Fix loo test

--- tests/testthat/test-crossvalidate.R
+++ tests/testthat/test-crossvalidate.R
@@ -5,18 +5,25 @@ common_warn <- "(not converge)|(0 or 1)|(scale = TRUE)"
 test_that("output has expected structure", {
   cv <- suppressWarnings(crossvalidate(nhanes_mids, "hyp", k=2))
   expect_s3_class(cv, "cv")
-  expect_equal(nrow(cv), nrow(nhanes_large))
   expect_length(cv, 2)
-  expect_named(cv, c("prediction", "se"))
-  expect_gte(min(cv$prediction, na.rm=TRUE), 0)
-  expect_lte(max(cv$prediction, na.rm=TRUE), 1)
+  expect_named(cv, c("pooled", "imputed"))
+  expect_equal(nrow(cv$pooled), nrow(nhanes_large))
+  expect_length(cv$pooled, 2)
+  expect_named(cv$pooled, c("prediction", "se"))
+  expect_gte(min(cv$pooled$prediction, na.rm=TRUE), 0)
+  expect_lte(max(cv$pooled$prediction, na.rm=TRUE), 1)
+  expect_equal(sum(sapply(cv$imputed, nrow)), nrow(nhanes_large)*nhanes_mids$m)
+  expect_length(cv$imputed, nhanes_mids$m)
+  expect_named(cv$imputed[[1]], c("hyp", "prediction", "se"))
+  expect_gte(min(cv$imputed[[1]]$prediction, na.rm=TRUE), 0)
+  expect_lte(max(cv$imputed[[1]]$prediction, na.rm=TRUE), 1)
 })
 
 test_that("leave-one-out works", {
   skip_on_ci()
   skip_on_cran()
   expect_warning(cv <- crossvalidate(nhanes_mids, "hyp", k=100), common_warn)
-  expect_equal(nrow(cv), nrow(nhanes_large))
+  expect_length(cv$imputed, nrow(nhanes_large))
 })
 
 test_that("low number of outcomes is flagged", {
