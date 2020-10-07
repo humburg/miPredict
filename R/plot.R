da78cecdce12024af1ebe682719aecacc873e603
Peter Humburg <p.humburg@unsw.edu.au>
2020-10-07 23:36:49 +1100

    Add plotting function

---
+++ R/plot.R
@@ -0,0 +1,9 @@
+#' Create plots to assess predictive performance following cross-validation
+#' 
+#' @param x An object of class [cv].
+#' @param ... Further arguments are ignored.
+#' @method plot cv
+#' @importFrom classifierplots
+plot.cv <- function(x, ...) {
+  classifierplots(as.factor(x$pooled[[3]]), x$pooled$prediction)
+}
