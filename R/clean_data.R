d7ba033c39fd503f2d0ef769b0278eb117dfd1f7
Peter Humburg <p.humburg@unsw.edu.au>
2020-11-20 05:13:49 +1100

    Expand factors

--- R/clean_data.R
+++ R/clean_data.R
@@ -6,11 +6,21 @@
 #' factors converted to integer vectors.
 #' @note Currently only supports binary factors. Expansion into dummy variables is not performed.
 #' @export
-#' @importFrom dplyr mutate_if
-#' @importFrom dplyr %>%
 #' @examples
 #' library(mice)
 #' clean_data(nhanes2)
 clean_data <- function(data){
-  data %>% mutate_if(is.factor, ~as.integer(.)-1)
+  factors <- which(sapply(data, is.factor))
+  if(length(factors)){
+    expanded <- vector(mode="list", length=length(factors))
+    names(expanded) <- names(data)[factors]
+    for(i in factors){
+      expanded[[i]] <- model.matrix(as.formula(paste("~ 0", names(expanded)[i], sep="+")))
+    }
+    expanded <- do.call(cbind, expanded)
+    data <- data[,-factors]
+    data <- cbind(data, expanded)
+  }
+  
+  data
 }
