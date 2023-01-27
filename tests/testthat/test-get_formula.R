485be2e6a71372f2d306cc3e38a44730a4a3126e
Peter Humburg <p.humburg@unsw.edu.au>
2023-01-27 15:42:18 +1100

    Add unit test for incorrect variable name

--- tests/testthat/test-get_formula.R
+++ tests/testthat/test-get_formula.R
@@ -1,5 +1,10 @@
 context("model formula")
 
 test_that("variable names for binary factors are correct",{
-  expect_equal(colnames(attr(terms(as.formula(get_formula("hyp", "bmi", nhanes_mids))), "factors")), "hyp")
+  expect_silent(form <- get_formula("hyp", "bmi", nhanes_mids))
+  expect_equal(colnames(attr(terms(as.formula(form)), "factors")), "hyp")
+})
+
+test_that("variable names for multi-level factors are correct",{
+  expect_equal(colnames(attr(terms(as.formula(get_formula("age", "bmi", nhanes_mids))), "factors")), c("age40.59", "age60.99"))
 })
