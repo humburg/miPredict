35778a206b494fadfe9efc00d8bf3b9bbac5f03b
Peter Humburg <peter.humburg@gmail.com>
2023-05-04 16:34:10 +1000

    Separate response and predictor names

--- R/select_variables.R
+++ R/select_variables.R
@@ -4,8 +4,9 @@
 select_variables <-
 function(data, response, family="binomial", include, ...){
   var_names <- names(data)[-(1:2)]
-  fit_formula <- as.formula(paste0(response, " ~ ", paste(var_names[var_names != response], collapse="+")))
-  penalty.factor = rep(1, length(var_names)-1)
+  var_names <- var_names[var_names != response]
+  fit_formula <- as.formula(paste0(response, " ~ ", paste(var_names, collapse="+")))
+  penalty.factor = rep(1, length(var_names))
   if(!missing(include) && !is.null(include) && length(include) > 0) {
     penalty.factor[var_names %in% include] <- 0
   }
