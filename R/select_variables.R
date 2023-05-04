368adbd57261ce1a244614bafd32dd1b8c6eb275
Peter Humburg <p.humburg@unsw.edu.au>
2023-05-04 10:24:03 +1000

    Don't count response in number of variables

--- R/select_variables.R
+++ R/select_variables.R
@@ -5,7 +5,7 @@ select_variables <-
 function(data, response, family="binomial", include, ...){
   var_names <- names(data)[-(1:2)]
   fit_formula <- as.formula(paste0(response, " ~ ", paste(var_names[var_names != response], collapse="+")))
-  penalty.factor = rep(1, length(var_names))
+  penalty.factor = rep(1, length(var_names)-1)
   if(!missing(include) && !is.null(include) && length(include) > 0) {
     penalty.factor[var_names %in% include] <- 0
   }
